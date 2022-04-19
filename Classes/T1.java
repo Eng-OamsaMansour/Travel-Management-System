c40447826adc50ce5b4ff16bb4733b25de1ea362
TasneemKhalil <103995984+TasneemKhalil@users.noreply.github.com>
2022-04-19 14:25:20 +0300

    up1

--- Classes/T1.java
+++ Classes/T1.java
@@ -8,6 +8,7 @@ public class T1 {
     public static void main(String[] args) {
         System.out.print("osama");
         System.out.print("batool");
-        //comment
+        System.out.println("Tasneem");
+        //comment 
     }
 }
--- test1.java
+++ test1.java
@@ -1,6 +1,9 @@
 public class first {
     
     public static void main(string[] args){
+
         System.out.print("Hello World");
+        System.out.println("ss1");
+        //comment
     }
-}} 
+}

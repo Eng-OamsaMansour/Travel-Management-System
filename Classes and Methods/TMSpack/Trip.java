ee88b6a19a15f1f9de6df2eb58601b42470f3509
Osama Mansour <osamanote2016@gmail.com>
2022-04-23 01:27:03 +0300

    SETTERS AND GETTERS FUNCTIONS FOR TRIP CLASS PRIVATE ATTRIBUTE

--- Classes and Methods/TMSpack/Trip.java
+++ Classes and Methods/TMSpack/Trip.java
@@ -1,7 +1,7 @@
 package TMSpack;
 import java.time.LocalDateTime;
 
-public class Trip<Hotel> {
+public class Trip {
 	private String trip_ID;
 	private String Location_ID;
 	private Hotel booked_hotel;
@@ -17,6 +17,62 @@ public class Trip<Hotel> {
 		this.date = date;
 		this.price = price;
 		this.transportation_way = transportation_way;
+		this.trip_path_discreption = trip_path_discreption;
+	}
+
+	public String getTrip_ID() {
+		return trip_ID;
+	}
+
+	public void setTrip_ID(String trip_ID) {
+		this.trip_ID = trip_ID;
+	}
+
+	public String getLocation_ID() {
+		return Location_ID;
+	}
+
+	public void setLocation_ID(String location_ID) {
+		Location_ID = location_ID;
+	}
+
+	public Hotel getBooked_hotel() {
+		return booked_hotel;
+	}
+
+	public void setBooked_hotel(Hotel booked_hotel) {
+		this.booked_hotel = booked_hotel;
+	}
+
+	public LocalDateTime getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDateTime date) {
+		this.date = date;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getTransportation_way() {
+		return transportation_way;
+	}
+
+	public void setTransportation_way(String transportation_way) {
+		this.transportation_way = transportation_way;
+	}
+
+	public String getTrip_path_discreption() {
+		return trip_path_discreption;
+	}
+
+	public void setTrip_path_discreption(String trip_path_discreption) {
 		this.trip_path_discreption = trip_path_discreption;
 	}
 

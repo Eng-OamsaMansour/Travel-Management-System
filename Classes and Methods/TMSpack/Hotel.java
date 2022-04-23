02974b2d791cb1924df36eb33f8d6596b2076bed
Osama Mansour <osamanote2016@gmail.com>
2022-04-23 04:11:33 +0300

    Hotel class constractor and attributs build

--- Classes and Methods/TMSpack/Hotel.java
+++ Classes and Methods/TMSpack/Hotel.java
@@ -1,2 +1,18 @@
 package TMSpack;
-intial statment
+
+package TMSpack;
+
+public class Hotel {
+	private String hotel_ID;
+	private int hotel_stars;
+	private String hotel_name;
+	private Locations hotel_address;
+	
+	public Hotel (String hotel_ID,int hotel_stars,String hotel_name,Locations hotel_address) {
+		this.hotel_ID = hotel_ID;
+		this.hotel_stars = hotel_stars;
+		this.hotel_name = hotel_name;
+		this.hotel_address = hotel_address;
+	}
+	
+}

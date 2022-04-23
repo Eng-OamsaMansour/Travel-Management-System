7ae06bbc201aebe69f1107611c1e7698e730b6aa
Osama Mansour <osamanote2016@gmail.com>
2022-04-23 04:13:44 +0300

    setters and getters for hotel class private attribute

--- Classes and Methods/TMSpack/Hotel.java
+++ Classes and Methods/TMSpack/Hotel.java
@@ -1,7 +1,5 @@
 package TMSpack;
 
-package TMSpack;
-
 public class Hotel {
 	private String hotel_ID;
 	private int hotel_stars;
@@ -15,4 +13,35 @@ public class Hotel {
 		this.hotel_address = hotel_address;
 	}
 	
+    public String getHotel_ID() {
+		return hotel_ID;
+	}
+
+	public void setHotel_ID(String hotel_ID) {
+		this.hotel_ID = hotel_ID;
+	}
+
+	public int getHotel_stars() {
+		return hotel_stars;
+	}
+
+	public void setHotel_stars(int hotel_stars) {
+		this.hotel_stars = hotel_stars;
+	}
+
+	public String getHotel_name() {
+		return hotel_name;
+	}
+
+	public void setHotel_name(String hotel_name) {
+		this.hotel_name = hotel_name;
+	}
+
+	public Locations getHotel_address() {
+		return hotel_address;
+	}
+
+	public void setHotel_address(Locations hotel_address) {
+		this.hotel_address = hotel_address;
+	}
 }

e9bffa62ddaa26cefbcae258df3e9702488afe64
Osama Mansour <osamanote2016@gmail.com>
2022-04-23 04:23:23 +0300

    Locations class constractor and attribute build

--- Classes and Methods/TMSpack/Locations.java
+++ Classes and Methods/TMSpack/Locations.java
@@ -1,2 +1,15 @@
 package TMSpack;
-intial statment
+
+import java.util.ArrayList;
+
+public class Locations {
+	private String country;
+	private String city;
+	private ArrayList<Hotel> avilable_hotels = new ArrayList<Hotel>();
+
+	public Locations(String country,String city,ArrayList<Hotel> avilable_hotels) {
+		this.avilable_hotels = avilable_hotels;
+		this.city = city;
+		this.country = country;
+	}
+}

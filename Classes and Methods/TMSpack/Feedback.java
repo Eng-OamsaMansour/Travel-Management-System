e5bb8c125d8a594aa6aaeca089668f33d98cb764
TasneemKhalil <103995984+TasneemKhalil@users.noreply.github.com>
2022-04-23 12:22:27 +0300

    Update Feedback.java

--- Classes and Methods/TMSpack/Feedback.java
+++ Classes and Methods/TMSpack/Feedback.java
@@ -4,10 +4,25 @@ public class Feedback {
     private String feedback_text;
 	private int feedback_ID;
 
-    public Feedback(){
+    public Feedback(String feedback_text,int feedback_ID){
+      this.feedback_ID=feedback_ID;
+      this.feedback_text=feedback_text;
 
     }
+    public int getFeedbackId() {
+        return feedback_ID;
+      }
+      public void setFeedbackId(int feedback_ID) {
+        this.feedback_ID = feedback_ID;
+      }
+      public String getFeedbackText() {
+        return feedback_text;
+      }
 
-    
+      public void setFeedbackText(String feedback_text) {
+        this.feedback_text = feedback_text;
+      }
+
+
     
 }

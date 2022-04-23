93418b4a9d50b9b662beed99f6a7f632136d02f9
Osama Mansour <osamanote2016@gmail.com>
2022-04-23 04:25:48 +0300

    setters and getters functions for Locations class private attribute

--- Classes and Methods/TMSpack/Locations.java
+++ Classes and Methods/TMSpack/Locations.java
@@ -12,4 +12,30 @@ public class Locations {
 		this.city = city;
 		this.country = country;
 	}
+    
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public ArrayList<Hotel> getAvilable_hotels() {
+		return avilable_hotels;
+	}
+
+	public void setAvilable_hotels(ArrayList<Hotel> avilable_hotels) {
+		this.avilable_hotels = avilable_hotels;
+	}
+	
+	
 }

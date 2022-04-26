7792cebad766d159b9d7011554e14a1eb05ab125
Batoolkharraz <batoolkhelf@gmail.com>
2022-04-26 23:16:52 +0300

    setters and getters for user class private attribute

---
+++ Classes and Methods/TMSpack/user.java
@@ -0,0 +1,29 @@
+package Classes and Methods.TMSpack;
+
+import javax.swing.*;
+public class user {
+
+	private String userID;
+
+
+public user(String userID){
+    this.userID=userID;
+}
+public void set_userID(String id) {
+	int dig = id.length();
+	if(dig==10)
+	userID=id;
+	else
+		JOptionPane.showMessageDialog(null, " Your ID number is UNCORRECT, Please enter your information again !! ");
+}
+
+public String get_userID() {
+	return userID;
+}	
+
+
+    
+}
+
+    
+

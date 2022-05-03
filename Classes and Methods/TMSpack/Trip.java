0b33984387c0c14b12613979bdb7f9cee75a15f2
Osama Mansour <osamanote2016@gmail.com>
2022-05-03 05:24:32 +0300

    Add Trip Function

--- Classes and Methods/TMSpack/Trip.java
+++ Classes and Methods/TMSpack/Trip.java
@@ -1,16 +1,25 @@
 package TMSpack;
 import java.time.LocalDateTime;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import oracle.jdbc.pool.OracleDataSource;
 
 public class Trip {
 	private String trip_ID;
 	private String Location_ID;
 	private Hotel booked_hotel;
-	private LocalDateTime date;
+	private Date date;
 	private int price;
 	private String transportation_way;
 	private String trip_path_discreption;
 	
-	public Trip(String ID,String Location_ID,Hotel booked_hotel,LocalDateTime date,int price,String transportation_way,String trip_path_discreption) {
+	public Trip(String ID,String Location_ID,Hotel booked_hotel,Date date,int price,String transportation_way,String trip_path_discreption) {
 		this.trip_ID = ID;
 		this.Location_ID = Location_ID;
 		this.booked_hotel = booked_hotel;
@@ -44,11 +53,11 @@ public class Trip {
 		this.booked_hotel = booked_hotel;
 	}
 
-	public LocalDateTime getDate() {
+	public Date getDate() {
 		return date;
 	}
 
-	public void setDate(LocalDateTime date) {
+	public void setDate(Date date) {
 		this.date = date;
 	}
 
@@ -76,4 +85,59 @@ public class Trip {
 		this.trip_path_discreption = trip_path_discreption;
 	}
 
+    public void Add_Trip(Trip newTRIP) {
+        ArrayList<String> IDs = new ArrayList<String>();
+        try {
+            OracleDataSource ods = new OracleDataSource();
+            ods.setURL("jdbc:oracle:thin:@localhost:1521:xe");
+            ods.setUser("c##TMS");
+            ods.setPassword("123456");
+            Connection con = ods.getConnection();
+            Statement stmt = con.createStatement();
+            ResultSet result = stmt.executeQuery("select * from TRIP");
+            while (result.next()) {
+                IDs.add(result.getString(1));
+            }
+            con.close();
+        } catch (Exception e) {
+            System.out.print(e.toString());
+        }
+        String graterid = IDs.get(0);
+        for (int i = 0; i < IDs.size(); i++) {
+            int condition = graterid.compareTo(IDs.get(i));
+            if (condition < 0) {
+                graterid = IDs.get(i);
+            }
+        }
+        String[] splited_id = graterid.split("T", 2);
+        String id_num_part = splited_id[1];
+        int INT_ID = Integer.parseInt(id_num_part);
+        INT_ID++;
+        String id = "0";
+        id = "T"+Integer.toString(INT_ID);
+         Driver driver = new oracle.jdbc.driver.OracleDriver();
+        try {
+            DriverManager.registerDriver(driver);
+            String url = "jdbc:oracle:thin:@localhost:1521:xe";
+            Connection con = DriverManager.getConnection(url, "c##TMS", "123456");           
+            /*(TRIP_ID, DATE_, PRICE, TRIP_LOCATION, TRIP_HOTEL, TRANSPORTATION_WAY, TRIP_PATH_DESCRIPTION, TRIP_KIND, MAX_BOOKING_NUM, CURRENT_BOOKING_NUMBER)*/          
+            PreparedStatement stmt = con.prepareStatement("insert into TRIP values(?,?,?,?,?,?,?,?,?,?)");
+            stmt.setString(1, id);
+            stmt.setDate(2, newTRIP.date);
+            stmt.setInt(3, newTRIP.price);
+            stmt.setString(4, newTRIP.Location_ID);
+            stmt.setString(5, newTRIP.booked_hotel.getHotel_ID());
+            stmt.setString(6, newTRIP.transportation_way);
+            stmt.setString(7, newTRIP.trip_path_discreption);
+            stmt.setString(8, newTRIP.Trip_Kind);
+            stmt.setInt(9, newTRIP.max_booking_num);
+            stmt.setInt(10, newTRIP.current_booking_num);
+            stmt.executeUpdate();
+            con.setAutoCommit(false);
+            con.commit();
+            con.close();
+        } catch (Exception ex) {            
+           System.out.print(ex.toString());
+        }            
+    }
 }

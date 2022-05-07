a4cd573125431c970a0e1ade761af1acd0536eca
TasneemKhalil <103995984+TasneemKhalil@users.noreply.github.com>
2022-05-07 18:41:55 +0300

    Update Feedback.java

--- Classes and Methods/TMSpack/Feedback.java
+++ Classes and Methods/TMSpack/Feedback.java
@@ -1,4 +1,14 @@
 package TMSpack;
+import java.time.LocalDateTime;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import oracle.jdbc.pool.OracleDataSource;
 
 public class Feedback {
     private String feedback_text;
@@ -23,7 +33,55 @@ public class Feedback {
         this.feedback_text = feedback_text;
       }
 
-     
+      public void addFeedback(Feedback newUSER_FEEDBACK) {
+        ArrayList<String> IDs = new ArrayList<String>();
+        try {
+            OracleDataSource ods = new OracleDataSource();
+            ods.setURL("jdbc:oracle:thin:@localhost:1521:xe");
+            ods.setUser("c##TMS");
+            ods.setPassword("123456");
+            Connection con = ods.getConnection();
+            Statement stmt = con.createStatement();
+            ResultSet result = stmt.executeQuery("select * from USER_FEEDBACK");
+            while (result.next()) {
+                IDs.add(result.getString(1));
+            }
+            con.close();
+        } catch (Exception e) {
+            System.out.print(e.toString());
+        }
+        String graterid = IDs.get(0);
+        for (int i = 0; i < IDs.size(); i++) {
+            int condition = graterid.compareTo(IDs.get(i));
+            if (condition < 0) {
+                graterid = IDs.get(i);
+            }
+        }
+        String[] splited_id = graterid.split("F", 2);
+        String id_num_part = splited_id[1];
+        int INT_ID = Integer.parseInt(id_num_part);
+        INT_ID++;
+        String id = "0";
+        id = "F"+Integer.toString(INT_ID);
+         Driver driver = new oracle.jdbc.driver.OracleDriver();
+        try {
+            DriverManager.registerDriver(driver);
+            String url = "jdbc:oracle:thin:@localhost:1521:xe";
+            Connection con = DriverManager.getConnection(url, "c##TMS", "123456");           
+                     
+            PreparedStatement stmt = con.prepareStatement("insert into USER_FEEDBACK values(?,?,?)");
+            stmt.setString(1, id);
+            stmt.setString(2, newUSER_FEEDBACK.feedback_ID);
+            stmt.setString(3, newUSER_FEEDBACK.feedback_text);
+            
+            stmt.executeUpdate();
+            con.setAutoCommit(false);
+            con.commit();
+            con.close();
+        } catch (Exception ex) {            
+           System.out.print(ex.toString());
+        }            
+    }
 
 
 

e1c476a7eca8dae8de38a970716c1e8695c6c1b1
Batoolkharraz <batoolkhelf@gmail.com>
2022-05-07 15:11:38 +0300

    update file

--- Classes and Methods/TMSpack/person.java
+++ Classes and Methods/TMSpack/person.java
@@ -3,70 +3,86 @@ import javax.swing.*;
 
 
 public class person {
-private String name;
-private String ID;
-private String age;
+private String Fname;
+private String Lname;
+private String GovID;
+private int age;
 private String phonenum;
+private String username;
+private String password;
 
-public person(String name, String ID, String age, String phonenum){
-	this.name=name;
-	this.ID=ID;
-	this.age=age;
-	this.phonenum=phonenum;
+
+public person() {
 
 }
-/*
-public person(String NAME, String I, String AGE, String PHONENUM){
 
-	name=NAME;
-	ID=I;
-	age=AGE;
-	phonenum=PHONENUM;
+public person(String Fname, String Lname, String GovID, int age, String phonenum, String username, String password) {
+	this.Fname = Fname;
+	this.Lname = Lname;
+	this.GovID = GovID;
+	this.age = age;
+	this.phonenum = phonenum;
+	this.username = username;
+	this.password = password;
 
-}*/
-
-public void set_name(String n) {
-	name=n;
 }
 
-public String get_name() {
-	return name;
+public String getFname() {
+	return Fname;
 }
 
-public void set_ID(String id) {
-	int dig = id.length(); // to check the number of digits in ID number 
-	if(dig==10)
-	ID=id;
-	else
-		JOptionPane.showMessageDialog(null, " Your ID number is UNCORRECT, Please enter your information again !! ");
+public void setFname(String n) {
+	Fname=n;
 }
 
-public String get_ID() {
-	return ID;
+public String getLname() {
+	return Lname;
 }
 
-public void set_age(String a) {
-	int aa= 
-	if (a>=18)	
+public void setLname(String Lname) {
+	this.Lname = Lname;
+}
+
+public void setGovID(String id) {
+
+	GovID=id;
+}
+
+public String getGovID() {
+	return GovID;
+}
+
+public void setAge(int a) {	
 	age=a;
-	else 
-		JOptionPane.showMessageDialog(null, " Your AGE is UNDER 18 , Please enter your information again !! ");	
 }
 
-public int get_age() {
+public int getAge() {
 	return age;
 }
 
-public void set_phonenum(int num) {
-	int N = String.valueOf(num).length(); // to check the number of digits in phone number 
-	if(N==5)
+public void setPhonenum(String num) {
 	phonenum=num;
-	else
-		JOptionPane.showMessageDialog(null, " Your PHONE NUMBER is less than 10 digit , Please enter your information again !! ");
 }
 
-public int get_phonenum() {
+public String getPhonenum() {
 	return phonenum;
+}
+
+
+public String getUsername() {
+	return username;
+}
+
+public void setUsername(String username) {
+	this.username = username;
+}
+
+public String getPassword() {
+	return password;
+}
+
+public void setPassword(String password) {
+	this.password = password;
 }
 
 

30663f37d3c3e05025b237140ad1ff51753bfccb
Batoolkharraz <batoolkhelf@gmail.com>
2022-05-07 15:10:26 +0300

    update

---
+++ Classes and Methods/TMSpack/person.java
@@ -0,0 +1,73 @@
+
+import javax.swing.*;
+
+
+public class person {
+private String name;
+private String ID;
+private String age;
+private String phonenum;
+
+public person(String name, String ID, String age, String phonenum){
+	this.name=name;
+	this.ID=ID;
+	this.age=age;
+	this.phonenum=phonenum;
+
+}
+/*
+public person(String NAME, String I, String AGE, String PHONENUM){
+
+	name=NAME;
+	ID=I;
+	age=AGE;
+	phonenum=PHONENUM;
+
+}*/
+
+public void set_name(String n) {
+	name=n;
+}
+
+public String get_name() {
+	return name;
+}
+
+public void set_ID(String id) {
+	int dig = id.length(); // to check the number of digits in ID number 
+	if(dig==10)
+	ID=id;
+	else
+		JOptionPane.showMessageDialog(null, " Your ID number is UNCORRECT, Please enter your information again !! ");
+}
+
+public String get_ID() {
+	return ID;
+}
+
+public void set_age(String a) {
+	int aa= 
+	if (a>=18)	
+	age=a;
+	else 
+		JOptionPane.showMessageDialog(null, " Your AGE is UNDER 18 , Please enter your information again !! ");	
+}
+
+public int get_age() {
+	return age;
+}
+
+public void set_phonenum(int num) {
+	int N = String.valueOf(num).length(); // to check the number of digits in phone number 
+	if(N==5)
+	phonenum=num;
+	else
+		JOptionPane.showMessageDialog(null, " Your PHONE NUMBER is less than 10 digit , Please enter your information again !! ");
+}
+
+public int get_phonenum() {
+	return phonenum;
+}
+
+
+}

17761f7309b34f2b009533a1889a7dd8b16986b5
Batoolkharraz <batoolkhelf@gmail.com>
2022-05-07 14:36:46 +0300

    Create user.java

---
+++ Classes and Methods/TMSpack/TMSpack/user.java
@@ -0,0 +1,25 @@
+package TMSpack;
+
+public class user extends person {
+
+	private String userID;
+	person p;
+
+
+public user(String userID){
+    this.userID=userID;
+}
+public void set_userID(String id) {
+	userID=id;
+}
+
+public String get_userID() {
+	return userID;
+}	
+
+
+    
+}
+
+    
+
